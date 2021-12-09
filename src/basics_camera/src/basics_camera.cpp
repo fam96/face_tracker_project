094f64971d39d6f69c8652c31ed004005e9c9860
Francisco Martinez <francisco.a.martinez.96@gmail.com>
2021-12-08 21:57:22 -0300

    funcion de deteccion

--- src/basics_camera/src/basics_camera.cpp
+++ src/basics_camera/src/basics_camera.cpp
@@ -20,10 +20,48 @@ using namespace cv;
 
 image_transport::Subscriber image_sub_;
 image_transport::Publisher image_pub_;
-string input_image_topic, output_image_topic;
-bool display_input_image, display_output_image;
-int screenmaxx;
+string input_image_topic, output_image_topic,haar_file_face;
+bool display_input_image, display_output_image,face_tracking;
+int screenmaxx,center_offset;
 
+//funcion de deteccion de caras
+void Detection( Mat& img, CascadeClassifier& cascade)
+{
+    double t = 0;
+    double scale = 1;
+    vector<Rect> faces, faces2;
+    Mat gray, smallImg;
+    //convierte la imagen de colores a escala de grises
+    cvtColor( img, gray, COLOR_BGR2GRAY );
+    double fx = 1 / scale ;
+    //redimensionamos la imagen con una interpolacion bilineal
+    resize( gray, smallImg, Size(), fx, fx, INTER_LINEAR );
+    //genera una ecualizacion de un histograma, para mejorar el contraste y brillo
+    equalizeHist( smallImg, smallImg );
+
+    //generamos una deteccion de caras sobre la imagen de entrada
+    cascade.detectMultiScale( smallImg, faces,1.1, 15, 0|CASCADE_SCALE_IMAGE,Size(30, 30) );
+
+    //generamos las marcas en la imagen de salida
+    for ( size_t i = 0; i < faces.size(); i++ )
+    {
+        //se toma una cara
+        Rect r = faces[i];
+        int x,y;
+
+        double aspect_ratio = (double)r.width/r.height;
+
+        if( 0.75 < aspect_ratio && aspect_ratio < 1.3 )
+        {
+          x = cvRound((r.x + r.width*0.5)*scale);
+          y = cvRound((r.y + r.height*0.5)*scale);
+
+          ROS_INFO("X: %d - Y: %d - Face: %d",x,y,i);
+        }
+    }
+}
+
+//funcion callback del topico imagen
 void imageCb(const sensor_msgs::ImageConstPtr& msg){
 
     cv_bridge::CvImagePtr cv_ptr;
@@ -41,10 +79,20 @@ void imageCb(const sensor_msgs::ImageConstPtr& msg){
       return;
     }
 
+    //Cargamos el clasificador de cascada
+    string cascadeName = haar_file_face;
+    CascadeClassifier cascade;
+	  if( !cascade.load( cascadeName ) )
+	  {
+		  ROS_ERROR("ERROR: Could not load classifier cascade");
+      return;
+	  }
+
     //Mostrar la imagen original en una ventana
 	  if (display_input_image == true){
 	    imshow("Input Image", cv_ptr->image);
 	  }
+    Detection(cv_ptr->image,cascade);
     //Mostrar la imagen de salida en una ventana
     if (display_output_image == true){
     	imshow( "Output Image", cv_ptr->image);
@@ -56,7 +104,8 @@ void imageCb(const sensor_msgs::ImageConstPtr& msg){
     waitKey(30);
 }
 
-void ReadAndShow(){
+//funcion de suscripcion y publicacion de topicos y carga de parametros
+void SubAndPub(){
   
   ros::NodeHandle nh_;
   image_transport::ImageTransport it_(nh_);
@@ -65,19 +114,29 @@ void ReadAndShow(){
   input_image_topic = "/raspicam_node/image";
   //topico de imagenes de salida
   output_image_topic = "/face_detector/raw_image";
+  //direccion de algoritmo de rastreo de caras
+  haar_file_face = "/data/face.xml"; 
   //booleano para abrir la ventana con la imagen original
   display_input_image = true;
   //booleano para abrir la ventana con la imagen de salida  
   display_output_image = true;
-
+  //tamaño maximo de la imagen?
   screenmaxx = 640;
+  //offset de la cara al centro de la imagen
+  center_offset=100;
 
+  //Cargamos los parametros necesarios
   try{
     nh_.getParam("image_input_topic", input_image_topic);
     nh_.getParam("face_detected_image_topic", output_image_topic);
     nh_.getParam("display_original_image", display_input_image);
     nh_.getParam("display_tracking_image", display_output_image);
+    nh_.getParam("center_offset", center_offset);
     nh_.getParam("screenmaxx", screenmaxx);
+
+    nh_.getParam("haar_file_face", haar_file_face);
+    nh_.getParam("face_tracking", face_tracking);
+    
 
     ROS_INFO("Successfully Loaded tracking parameters");
   }
@@ -97,7 +156,7 @@ void ReadAndShow(){
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "basics_camera");
-  ReadAndShow();
+  SubAndPub();
   ros::spin();
   return 0;
 }
